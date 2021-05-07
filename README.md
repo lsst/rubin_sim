bb084faeda7885983912ff123cca881177f22ac6
Peter Yoachim <yoachim@uw.edu>
2021-05-07 14:07:20 -0700

    starting data download tools

--- README.md
+++ README.md
@@ -5,4 +5,21 @@ Scheduler, survey strategy analysis, and other simulation tools for Rubin Observ
 
 # Installation
 
-Clone the repo and use the following to develop:  `pip install -e .`
+
+Install anaconda python
+
+set up a new environemnt with:
+`conda create -n rubin scipy numpy healpy astropy pandas jupyterlab sqlite palpy matplotlib`
+
+activate the new enviroment:
+`conda activate rubin`
+
+pip install pyephem:  `pip install pyephem`
+
+Clone the repo and use the following to install and develop:  
+```
+git clone git@github.com:lsst/rubin_sim.git
+cd rubin_sim
+pip install -e .
+```
+
---
+++ rubin_sim/data/data_sets.py
@@ -0,0 +1,71 @@
+import os
+import warnings
+import subprocess
+
+
+__all__ = ['get_data_dir', 'check_and_load', 'rsync_sims_data']
+
+
+def get_data_dir():
+    """Get the location of the rubin_sim data directory.
+
+    Returns
+    -------
+    string that is the path to the root data directory
+    """
+
+    # See if there is an environment variable with the path
+    data_dir = os.getenv('RUBIN_SIM_DATA_DIR')
+
+    # Set the root data directory
+    if data_dir is None:
+        warnings.warn('No RUBIN_SIM_DATA_DIR env variable, using $HOME/rubin_sim_data')
+        data_dir = os.path.join(os.getenv('HOME'), 'rubin_sim_data')
+    return data_dir
+
+
+def check_and_load(subdir, **kwargs):
+    """Check if data files exist, download if needed
+    """
+    data_root = get_data_dir()
+    path = os.path.join(data_root, subdir)
+    if os.path.isdir(path):
+        nfiles = len(os.listdir(path))
+    else:
+        nfiles = 0
+
+    if nfiles == 0:
+        rsync_sims_data(subdirs=[subdir], **kwargs)
+
+
+def rsync_sims_data(data_dir=None, remote_location="lsst-rsync.ncsa.illinois.edu::sim/",
+                    subdirs=['maps', 'orbit_files', 'throughputs']):
+    """Download data files from NCSA.
+    Parameters
+    ----------
+    data_dir : str (None)
+        The path to where the rubin_sim data is stored. If None, checks if it's defined by environ variable.
+    remote_location : str (lsst-rsync.ncsa.illinois.edu::sim/)
+        The path the download data from
+    subdirs = list of str
+        The names of the subdirectories to syncronize. Options are XXX
+    """
+
+    if data_dir is None:
+        data_dir = get_data_dir()
+
+    # Check that the directory exists
+    if not os.path.isdir(data_dir):
+        warnings.warn('Directory %s does not exist, attempting to create' % data_dir)
+        os.mkdir(data_dir)
+
+    base_call = ["rsync", "-avz", "--progress", "--delete"]
+
+    for subdir in subdirs:
+        # Check that subdir exists
+        data_path = os.path.join(data_dir, subdir)
+        if not os.path.isdir(data_path):
+            os.mkdir(data_path)
+
+        call = base_call + [os.path.join(remote_location, subdir)] + [data_path]
+        subprocess.call(call)
