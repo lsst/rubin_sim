9905ef2eea732384a0db20684a9de034699fe980
Peter Yoachim <yoachim@uw.edu>
2021-05-18 14:54:03 -0700

    fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='rubin_sim',
       version='0.01',
       scripts=['bin/maf/addRun', 'bin/maf/glance_dir',
-               'bin/maf/maf_night_report', 'bin/maf/scimaf_dir', 'bin/maf/showMaf', 
-               'bin/rs_dowload_data'],
+               'bin/maf/maf_night_report', 'bin/maf/scimaf_dir', 'bin/maf/showMaf',
+               'bin/rs_download_data'],
       packages=['rubin_sim', ],
       )
