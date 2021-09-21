5746be3e6950633cab21301144b76e544fa378d5
Peter Yoachim <yoachim@uw.edu>
2021-09-21 11:32:05 -0700

    comments from review

--- rubin_sim/maf/metrics/brownDwarfMetric.py
+++ rubin_sim/maf/metrics/brownDwarfMetric.py
@@ -5,65 +5,80 @@ import rubin_sim.maf.utils as mafUtils
 from scipy import interpolate
 
 
+__all__ = ['BDParallaxMetric', 'VolumeSumMetric']
+
+
+def bd_colors(spec_type):
+    """Returns dictionary of potential Brown Dwarf colors
+
+    Parameters
+    ----------
+    spec_type : `str`
+        The desired spetral type of the Brown Dwarf.
+
+    """
+    result = {}
+    result['L0'] = {'i': 16.00, 'z': 14.52, 'y': 13.58}
+    result['L1'] = {'i': 16.41, 'z': 14.93, 'y': 13.97}
+    result['L2'] = {'i': 16.73, 'z': 15.30, 'y': 14.33}
+    result['L3'] = {'i': 17.4, 'z': 15.88, 'y': 14.89},
+    result['L4'] = {'i': 18.35, 'z': 16.68, 'y': 15.66}
+    result['L5'] = {'i': 18.71, 'z': 16.94, 'y': 15.87}
+    result['L6'] = {'i': 19.27, 'z': 17.35, 'y': 16.27}
+    result['L7'] = {'i': 20.09, 'z': 18.18, 'y': 17.13}
+    result['L8'] = {'i': 20.38, 'z': 18.10, 'y': 17.04}
+    result['L9'] = {'i': 20.09, 'z': 17.69, 'y': 16.57}
+    result['T0'] = {'i': 20.22, 'z': 17.98, 'y': 16.77}
+    result['T1'] = {'i': 21.10, 'z': 18.84, 'y': 17.45}
+    result['T2'] = {'i': 21.97, 'z': 18.26, 'y': 16.75}
+    result['T3'] = {'i': 22.50, 'z': 18.08, 'y': 16.50}
+    result['T4'] = {'i': 22.50, 'z': 18.02, 'y': 16.32}
+    result['T5'] = {'i': 22.69, 'z': 19.20, 'y': 17.43}
+    result['T6'] = {'i': 23.00, 'z': 19.82, 'y': 18.06}
+    result['T7'] = {'z': 21.17, 'y': 19.34}
+    result['T8'] = {'z': 21.52, 'y': 19.75}
+    result['T9'] = {'z': 21.82, 'y': 20.37}
+    return result[spec_type]
+
+
 class BDParallaxMetric(BaseMetric):
     """Calculate the distance to which one could reach a parallax SNR for a given object
-    Modification of ParallaxMetric. Taken from:
+    Modification of ParallaxMetric, illustrated in 
     https://github.com/jgizis/LSST-BD-Cadence/blob/main/bd_allLT_baseline_17.ipynb
 
     Uses columns ra_pi_amp and dec_pi_amp, calculated by the ParallaxFactorStacker.
 
     Parameters
     ----------
-    metricName : str, opt
+    metricName : `str`, opt
         Default 'parallax'.
-    m5Col : str, opt
+    m5Col : `str`, opt
         The default column name for m5 information in the input data. Default fiveSigmaDepth.
-    filterCol : str, opt
+    filterCol : `str`, opt
         The column name for the filter information. Default filter.
-    seeingCol : str, opt
+    seeingCol : `str`, opt
         The column name for the seeing information. Since the astrometry errors are based on the physical
         size of the PSF, this should be the FWHM of the physical psf. Default seeingFwhmGeom.
-    mags : dict
+    mags : `dict` (None)
         The absolute magnitude of the obeject in question. Keys of filter name, values in mags.
+        Defaults to an L7 spectral type if None.
     distances : np.array
         Distances to try putting the object at (pc).
-    atm_err : float, opt
+    atm_err : `float`, opt
         The expected centroiding error due to the atmosphere, in arcseconds. Default 0.01.
-    badval : float, opt
+    badval : `float`, opt
         The value to return when the metric value cannot be calculated. Default 0.
-
-    Good values for mags:
-    mags={'i': 16.00, 'z': 14.52, 'y': 13.58}, metricName='L0'
-    mags={'i': 16.41, 'z': 14.93, 'y': 13.97}, metricName='L1'
-    mags={'i': 16.73, 'z': 15.30, 'y': 14.33}, metricName='L2'
-    mags={'i': 17.4, 'z': 15.88, 'y': 14.89}, metricName='L3'
-    mags={'i': 18.35, 'z': 16.68, 'y': 15.66}, metricName='L4'
-    mags={'i': 18.71, 'z': 16.94, 'y': 15.87}, metricName='L5'
-    mags={'i': 19.27, 'z': 17.35, 'y': 16.27}, metricName='L6'
-    mags={'i': 20.09, 'z': 18.18, 'y': 17.13}, metricName='L7'
-    mags={'i': 20.38, 'z': 18.10, 'y': 17.04}, metricName='L8'
-    mags={'i': 20.09, 'z': 17.69, 'y': 16.57}, metricName='L9'
-    mags={'i': 20.22, 'z': 17.98, 'y': 16.77}, metricName='T0'
-    mags={'i': 21.10, 'z': 18.84, 'y': 17.45}, metricName='T1'
-    mags={'i': 21.97, 'z': 18.26, 'y': 16.75}, metricName='T2'
-    mags={'i': 22.50, 'z': 18.08, 'y': 16.50}, metricName='T3'
-    mags={'i': 22.50, 'z': 18.02, 'y': 16.32}, metricName='T4'
-    mags={'i': 22.69, 'z': 19.20, 'y': 17.43}, metricName='T5'
-    mags={'i': 23.00, 'z': 19.82, 'y': 18.06}, metricName='T6'
-    mags={'z': 21.17, 'y': 19.34}, metricName='T7'
-    mags={'z': 21.52, 'y': 19.75}, metricName='T8'
-    mags={'z': 21.82, 'y': 20.37}, metricName='T9'
     """
     def __init__(self, metricName='bdParallax', m5Col='fiveSigmaDepth',
                  filterCol='filter', seeingCol='seeingFwhmGeom',
-                 badval=0, mags={'i': 20.09, 'z': 18.18, 'y': 17.13}, parallax_snr=10.,
+                 badval=0, mags=None, parallax_snr=10.,
                  distances=np.arange(10, 200, 10),
                  atm_err=0.01, normalize=False, **kwargs):
-        Cols = [m5Col, filterCol, seeingCol, 'ra_pi_amp', 'dec_pi_amp']
+        cols = [m5Col, filterCol, seeingCol, 'ra_pi_amp', 'dec_pi_amp']
         
         units = 'pc'
-        super(BDParallaxMetric, self).__init__(Cols, metricName=metricName, units=units,
-                                               badval=badval, **kwargs)
+        super().__init__(cols, metricName=metricName, units=units,
+                         badval=badval, **kwargs)
         # set return types
         self.m5Col = m5Col
         self.seeingCol = seeingCol
@@ -71,6 +86,8 @@ class BDParallaxMetric(BaseMetric):
         self.distances = distances
         self.mags = {}
         distance_mod = 5.0*np.log10(distances)-5.0
+        if mags is None:
+            mags = bd_colors('L7')
         for key in mags:
             self.mags[key] = mags[key] + distance_mod
         self.parallax_snr_goal = parallax_snr
