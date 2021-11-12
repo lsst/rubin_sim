db6136afa7f907196bf0a9383e98ca570a577426
Lynne Jones <ljones.uw@gmail.com>
2021-11-11 22:04:46 -0800

    Add AGN Time Lag metric

---
+++ rubin_sim/maf/metrics/agnTimeLagMetric.py
@@ -0,0 +1,57 @@
+import numpy as np
+from .baseMetric import BaseMetric
+
+__all__ = ['AGN_TimeLagMetric']
+
+
+class AGN_TimeLagMetric(BaseMetric):
+    def __init__(self, lag=100, z=1, log=False, calcType='mean',
+                 mjdCol='observationStartMJD', filterCol='filter',
+                 metricName='AGN_TimeLag_Metric', **kwargs):
+        self.lag = lag
+        self.z = z
+        self.log = log
+        self.calcType = calcType
+        self.mjdCol = mjdCol
+        self.filterCol = filterCol
+        super().__init__(col=[self.mjdCol, self.filterCol], metricName=metricName, **kwargs)
+
+    # Calculate NQUIST value for time-lag and sampling time (redshift is included in formula if desired)
+    def _getNquistValue(self, caden, lag, z):
+        return (lag / ((1 + z) * caden))
+
+    def run(self, dataSlice, slicePoint=None):
+        # Calculate differences in time between visits
+        mv = np.sort(dataSlice[self.mjdCol])
+        val = np.diff(mv)
+        # If there was only one visit; bail out now.
+        if len(val) == 0:
+            return self.badval
+
+        # Otherwise summarize the time differences as:
+        if self.calcType == 'mean':
+            val = np.mean(val)
+        elif self.calcType == 'min':
+            val = np.min(val)
+        elif self.calcType == 'max':
+            val = np.max(val)
+        else:
+            # find the greatest common divisor
+            val = np.rint(val).astype(int)
+            val = np.gcd.reduce(val)
+
+        # Will always have a value at this point
+        nquist = self._getNquistValue(val, self.lag, self.z)
+        if self.log:
+            nquist = np.log(nquist)
+
+        # Threshold nquist value is 2.2,
+        # hence we are aiming to show values higher than threshold (2.2) value
+        threshold = 2.2
+        if self.log:
+            threshold = np.log(threshold)
+
+        if nquist < threshold:
+            nquist = self.badval
+
+        return nquist
