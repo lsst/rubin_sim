cef9fe20e6b7bdf787aecea60725f9d857f91a43
Peter Yoachim <yoachim@uw.edu>
2022-01-03 18:21:01 -0800

    another try to run on CI

--- rubin_sim/utils/getPackageDir.py
+++ rubin_sim/utils/getPackageDir.py
@@ -8,8 +8,11 @@ __all__ = ["getPackageDir"]
 def getPackageDir(package):
     """Return the path to a package"""
 
-    # See if removing $PREFIX let's the CI run it.
-    result = os.path.dirname(os.path.dirname(inspect.getfile(package))).replace(
-        "$PREFIX", ""
-    )
+    cwd = os.getcwd()
+    # If we are running at the top level, like the CI, do nothing
+    # otherwise, find the full path
+    if os.path.split(cwd)[-1] == "rubin_sim":
+        result = ""
+    else:
+        result = os.path.dirname(os.path.dirname(inspect.getfile(package)))
     return result
