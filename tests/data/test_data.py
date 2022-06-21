26a197766bd9e0fe18bffcba31712811b6c7b4d6
Peter Yoachim <yoachim@uw.edu>
2022-06-21 14:27:12 -0700

    update data file and add test

---
+++ tests/data/test_data.py
@@ -0,0 +1,18 @@
+import unittest
+from rubin_sim.data import (get_baseline, get_data_dir, data_versions)
+
+
+class DataTest(unittest.TestCase):
+
+    def testBaseline(self):
+        """
+        Get the baseline sim location
+        """
+    
+        baseline = get_baseline()
+        data_dir = get_data_dir()
+        versions = data_versions()
+
+
+if __name__ == "__main__":
+    unittest.main()
