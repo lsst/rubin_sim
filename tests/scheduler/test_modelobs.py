628134e82627503f72ed5211be8d2baffe89ad0c
Peter Yoachim <yoachim@uw.edu>
2023-06-15 16:28:35 -0700

    make it possible to replace seeing, clouds, downtime in ModelObservatory

---
+++ tests/scheduler/test_modelobs.py
@@ -0,0 +1,58 @@
+import numpy as np
+import unittest
+
+from rubin_sim.scheduler.model_observatory import ModelObservatory
+
+
+class KindaClouds(object):
+    """Dummy class that always sets the clouds level to a unique float"""
+
+    def __call__(self, mjd):
+        return 0.23659
+
+
+class ArbSeeing(object):
+    """Dummy class to always return a specific seeing value"""
+
+    def __call__(self, mjd):
+        fwhm_500 = 1.756978
+        return fwhm_500
+
+
+class TestModelObservatory(unittest.TestCase):
+    def test_replace(self):
+        """test that we can replace default downtimes, seeing, and clouds"""
+
+        mjd_start = 60111.0
+        mo_default = ModelObservatory(mjd_start=mjd_start)
+        cond_default = mo_default.return_conditions()
+
+        # Define new downtimes
+        downtimes = np.zeros(2, dtype=list(zip(["start", "end"], [float, float])))
+        downtimes["start"] = np.array([1, 10]) + mjd_start
+        downtimes["end"] = np.array([2, 11]) + mjd_start
+
+        seeing_data = ArbSeeing()
+        cloud_data = KindaClouds()
+
+        mo_new = ModelObservatory(
+            mjd_start=mjd_start,
+            seeing_data=seeing_data,
+            cloud_data=cloud_data,
+            downtimes=downtimes,
+        )
+        cond_new = mo_new.return_conditions()
+
+        # Make sure the internal downtimes are different
+        assert np.all(mo_default.downtimes != mo_new.downtimes)
+
+        # Make sure seeing is not the same
+        diff = cond_default.fwhm_eff["r"] - cond_new.fwhm_eff["r"]
+        assert np.nanmin(np.abs(diff)) > 0
+
+        # Make sure cloudyness level is not the same
+        assert cond_default.bulk_cloud != cond_new.bulk_cloud
+
+
+if __name__ == "__main__":
+    unittest.main()
