e76e7cde14bd88236429018ce82f016a6c6a1737
Eric Bellm <ecbellm@uw.edu>
2024-01-19 13:58:14 -0800

    add additional template quality info

--- rubin_sim/maf/metrics/incremental_template_metric.py
+++ rubin_sim/maf/metrics/incremental_template_metric.py
@@ -18,8 +18,12 @@ class TemplateTime(BaseMetric):
         Default 3. 
     seeing_percentile : `float`, opt
         Maximum percentile seeing to allow in the qualified images (0 - 100). 
+
+        Default 50.
     m5_percentile : `float`, opt
-        Maximum percentile m5 to allow in the qualified images (0 - 100). Stand in for `weight` in template.
+        Maximum percentile m5 to allow in the qualified images (0 - 100). 
+
+        Default 50.
     seeingCol : `str`, opt
         Name of the seeing column to use.
     m5Col : `str`, opt
@@ -32,7 +36,7 @@ class TemplateTime(BaseMetric):
         Name of column describing filter
     """
     
-    def __init__(self, n_images_in_template=3, seeing_percentile=25, m5_percentile=25, 
+    def __init__(self, n_images_in_template=3, seeing_percentile=50, m5_percentile=50, 
                  seeingCol='seeingFwhmEff', m5Col='fiveSigmaDepth',
                  nightCol = 'night', mjd_col = 'observationStartMJD', filter_col = 'filter', **kwargs):
         self.n_images_in_template = n_images_in_template
@@ -89,6 +93,17 @@ class TemplateTime(BaseMetric):
         N_images_with_template = np.sum(where_template)
         
         template_m5 = 1.25 * np.log10(np.sum(10.0 ** (0.8 * dataSlice[self.m5Col][idx_template_inputs])))
+
+        # derive variance-weighted PSF width
+        # 1. normalize the weights 
+        template_f0 = np.sqrt(np.sum(25 * 10 **  (0.8 * dataSlice[self.m5Col][idx_template_inputs])))
+        # 2. compute per-input noise
+        image_noise = template_f0 / 5 * 10 **  (-0.4 * dataSlice[self.m5Col])
+        # 3. used variance-weighted sum of squares to derive template seeing
+        template_seeing = np.sqrt(np.sum( 
+                            (dataSlice[self.seeingCol][idx_template_inputs]/
+                             image_noise[idx_template_inputs])**2.))
+
         
         delta_m5 = -2.5 * np.log10(np.sqrt(1.0 + 10 ** (-0.8 * (template_m5 - dataSlice[self.m5Col]))))
         diff_m5s = dataSlice[self.m5Col] + delta_m5
@@ -100,34 +115,47 @@ class TemplateTime(BaseMetric):
         result["N_images_until_template"] = idx_template_created + 1
         result["N_images_with_template"] = N_images_with_template
         result["Template_m5"] = template_m5
+        result["Template_seeing"] = template_seeing
         result["Total_alerts"] = np.sum(n_alerts_per_diffim[where_template])
         result["Template_input_m5s"] = dataSlice[self.m5Col][idx_template_inputs]
+        result["Template_input_seeing"] = dataSlice[self.seeingCol][idx_template_inputs]
+        result["Fraction_better_template_seeing"] = np.sum((dataSlice[self.seeingCol][where_template] > template_seeing)) / np.sum(where_template)
         result["Diffim_lc"] = {
             "mjd":dataSlice[self.mjd_col][where_template],
             "night":dataSlice[self.nightCol][where_template],
-            "diff_m5":diff_m5s[where_template],
             "band":dataSlice[self.filter_col][where_template],
+            "diff_m5":diff_m5s[where_template],
             "science_m5":dataSlice[self.m5Col][where_template],
+            "template_m5":template_m5 * np.ones(np.sum(where_template)),
+            "science_seeing":dataSlice[self.seeingCol][where_template],
+            "template_seeing":template_seeing * np.ones(np.sum(where_template)),
             "n_alerts":n_alerts_per_diffim[where_template]
         }
         
         return result
     
         
-    def reduceNight_template_created(self, metricVal): # returns night of template creation
+    def reduce_Night_template_created(self, metricVal): # returns night of template creation
         return metricVal["Night_template_created"]
     
-    def reduceN_nights_without_template(self, metricVal): # returns number of nights needed to complete template
+    def reduce_N_nights_without_template(self, metricVal): # returns number of nights needed to complete template
         return metricVal["N_nights_without_template"]
     
-    def reduceN_images_until_template(self, metricVal): # returns number of images needed to complete template
+    def reduce_N_images_until_template(self, metricVal): # returns number of images needed to complete template
         return metricVal["N_images_until_template"]
     
-    def reduceN_images_with_template(self, metricVal): # returns number of images with a template
+    def reduce_N_images_with_template(self, metricVal): # returns number of images with a template
         return metricVal["N_images_with_template"]
+
     
-    def reduceTemplate_m5(self, metricVal): # calculated coadded m5 of resulting template
+    def reduce_Template_m5(self, metricVal): # calculated coadded m5 of resulting template
         return metricVal["Template_m5"]
+
+    def reduce_Template_seeing(self, metricVal): # calculated seeing of resulting template
+        return metricVal["Template_seeing"]
     
-    def reduceTotal_alerts(self, metricVal):
+    def reduce_fraction_better_template_seeing(self, metricVal): # calculated seeing of resulting template
+        return metricVal["Fraction_better_template_seeing"]
+    
+    def reduce_Total_alerts(self, metricVal):
         return metricVal["Total_alerts"]
