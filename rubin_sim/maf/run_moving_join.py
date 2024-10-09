6c5330d608d97b0ab36c267135be03147e3b384c
Lynne Jones <ljones.uw@gmail.com>
2024-10-08 23:04:59 -0700

    Ruff run scripts

--- rubin_sim/maf/run_moving_join.py
+++ rubin_sim/maf/run_moving_join.py
@@ -40,12 +40,18 @@ def run_moving_join():
 
     # Outputs from the metrics are generally like so:
     #  <base_dir>/<splitDir>/<metricFileName>
-    # - base_dir tends to be <opsimName_orbitRoot> (but is set by user when starting to generate obs.)
-    # - splitDir tends to be <orbitRoot_split#> (and is set by observation generation script)
-    # - metricFile is <opsimName_metricName_metadata(NEO/L7/etc + metadata from metric script)_MOOB.npz
-    #  (the metricFileName is set by the metric generation script - run_moving_calc.py).
-    #  (note that split# does not show up in the metricFileName, and is not used in run_moving_calc.py).
-    #  ... this lets run_moving_calc.py easily run in parallel on multiple splits.
+    # - base_dir tends to be <opsimName_orbitRoot> (
+    # but is set by user when starting to generate obs.)
+    # - splitDir tends to be <orbitRoot_split#>
+    # (and is set by observation generation script)
+    # - metricFile is <opsimName_metricName_metadata
+    # (NEO/L7/etc + metadata from metric script)_MOOB.npz
+    #  (the metricFileName is set by the metric generation script
+    #  - run_moving_calc.py).
+    #  (note that split# does not show up in the metricFileName,
+    #  and is not used in run_moving_calc.py).
+    #  ... this lets run_moving_calc.py easily run in parallel on
+    #  multiple splits.
 
     # Assume splits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     splits = np.arange(0, 10, 1)
@@ -65,8 +71,10 @@ def run_moving_join():
     metricNames = []
     for m in metricfiles:
         mname = os.path.split(m)[-1]
-        # Hack out raw Discovery outputs. We don't want to join the raw discovery files.
-        # This is a hack because currently we're just pulling out _Time and _N_Chances to join.
+        # Hack out raw Discovery outputs.
+        # We don't want to join the raw discovery files.
+        # This is a hack because currently we're just pulling out
+        # _Time and _N_Chances to join but there are other options.
         if "Discovery" in mname:
             if "DiscoveryTime" in mname:
                 metricNames.append(mname)
