a9e6d35f42696f1b43774f251ede680dd4c55b38
Lynne Jones <ljones.uw@gmail.com>
2024-10-08 21:50:24 -0700

    Remove inits for rubin_scheduler moved modules

--- rubin_sim/site_models/__init__.py
+++
@@ -1,8 +0,0 @@
-import warnings
-
-from rubin_scheduler.site_models import *  # noqa: F403
-
-warnings.simplefilter("default")
-warnings.warn(
-    "rubin_sim.site_models is deprecated, switch to rubin_scheduler.site_models", DeprecationWarning
-)
