74cd507818d6234007b7df812f6d31e9fe4e894e
Lynne Jones <ljones.uw@gmail.com>
2024-11-13 14:15:14 -0800

    Black

--- rubin_sim/maf/batches/science_radar_batch.py
+++ rubin_sim/maf/batches/science_radar_batch.py
@@ -599,7 +599,7 @@ def science_radar_batch(
             maps_list=[dustmap],
             info_label=info_label,
             summary_metrics=summaryMetrics + [ThreebyTwoSummary],
-        #, ThreebyTwoSummary_simple],
+            # , ThreebyTwoSummary_simple],
             display_dict=displayDict,
         )
         displayDict["order"] += 1
