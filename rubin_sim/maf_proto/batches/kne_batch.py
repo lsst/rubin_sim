99e13b0c8757f3a65e0eb1ccfc9d2f04b3d70fce
Peter Yoachim <yoachim@uw.edu>
2025-05-09 15:54:16 -0700

    clean up

--- rubin_sim/maf_proto/batches/kne_batch.py
+++ rubin_sim/maf_proto/batches/kne_batch.py
@@ -52,21 +52,23 @@ def kne_batch(observations=None, run_name=None, quick_test=False, fig_saver=None
     metric = maf.KNePopMetric(mjd0=mjd0)
     if quick_test:
         metric.generate_kn_pop(n_events=100)
-    sl = maf.Slicer(nside=None, missing=0,
-                    ra=np.degrees(metric.ra),
-                    dec=np.degrees(metric.dec))
+    sl = maf.Slicer(nside=None, missing=0, ra=np.degrees(metric.ra), dec=np.degrees(metric.dec))
 
     kn_array, info = sl(visits_array, metric, info=info)
 
     ph = maf.PlotHealbin(info=info)
 
     for key in kn_array.dtype.names:
-        summary_stats.append(maf.gen_summary_row(info, 'Mean KNe ' + key, np.mean(kn_array[key])))
-        summary_stats.append(maf.gen_summary_row(info, 'Uncert in Mean KNe ' + key,
-                                                 np.mean(kn_array[key])/np.sqrt(np.sum(kn_array[key]))))
+        summary_stats.append(maf.gen_summary_row(info, "Mean KNe " + key, np.mean(kn_array[key])))
+        summary_stats.append(
+            maf.gen_summary_row(
+                info, "Uncert in Mean KNe " + key, np.mean(kn_array[key]) / np.sqrt(np.sum(kn_array[key]))
+            )
+        )
 
-    fig = ph(np.degrees(metric.ra), np.degrees(metric.dec), 
-             kn_array["multi_detect"], unit="KNe mean multi_detect")
+    fig = ph(
+        np.degrees(metric.ra), np.degrees(metric.dec), kn_array["multi_detect"], unit="KNe mean multi_detect"
+    )
     fig_saver(fig, info=info)
 
     return summary_stats
