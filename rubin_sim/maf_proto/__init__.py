50e20e03b6e6284287b7ad9204bdea028ff98984
Peter Yoachim <yoachim@uw.edu>
2025-08-29 16:18:53 -0700

    init

--- rubin_sim/maf_proto/__init__.py
+++ rubin_sim/maf_proto/__init__.py
@@ -2,7 +2,6 @@ from .batches import *
 from .db import *
 from .metrics import *
 from .plots import *
-from .run_comparison import *
 from .slicers import *
 from .utils import *
 from .web import *
