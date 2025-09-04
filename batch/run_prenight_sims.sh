106d367df23ef2c241f8a77921abee030192f235
Eric Neilsen <neilsen@fnal.gov>
2025-09-04 14:45:33 -0700

    Fix stderr of auxtel prenight as wel

--- batch/run_prenight_sims.sh
+++ batch/run_prenight_sims.sh
@@ -50,7 +50,7 @@ export PATH=${PACKAGE_DIR}/bin:${PATH}
 # Cannot get ts_fbs_utils from the EFD, so just guess the highest semantic version tag in the repo.
 # A "reference" can be a tag, hash, or branch.
 TS_FBS_UTILS_REFERENCE=$(curl -s https://api.github.com/repos/lsst-ts/ts_fbs_utils/tags | jq -r '.[].name' | egrep '^v[0-9]+.[0-9]+.[0-9]+$' | sort -V | tail -1)
-RUBIN_SIM_REFERENCE="v2.2.5.dev1"
+RUBIN_SIM_REFERENCE="v2.2.5.dev2"
 SCHEDVIEW_REFERENCE="v0.19.0.dev1"
 SIMS_SV_SURVEY_REFERENCE="v0.1.0"
 RUBIN_NIGHTS_REFERENCE="v0.3.0"
