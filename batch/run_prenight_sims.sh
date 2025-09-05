d9ed3ab1e8d4fd972df9d68d1359f1c6e40e3666
Eric Neilsen <neilsen@fnal.gov>
2025-09-04 19:38:57 -0700

    Update for sims_sv_survey v0.1.0.dev1

--- batch/run_prenight_sims.sh
+++ batch/run_prenight_sims.sh
@@ -50,9 +50,9 @@ export PATH=${PACKAGE_DIR}/bin:${PATH}
 # Cannot get ts_fbs_utils from the EFD, so just guess the highest semantic version tag in the repo.
 # A "reference" can be a tag, hash, or branch.
 TS_FBS_UTILS_REFERENCE=$(curl -s https://api.github.com/repos/lsst-ts/ts_fbs_utils/tags | jq -r '.[].name' | egrep '^v[0-9]+.[0-9]+.[0-9]+$' | sort -V | tail -1)
-RUBIN_SIM_REFERENCE="v2.2.5.dev2"
+RUBIN_SIM_REFERENCE="v2.2.5.dev3"
 SCHEDVIEW_REFERENCE="v0.19.0.dev1"
-SIMS_SV_SURVEY_REFERENCE="v0.1.0"
+SIMS_SV_SURVEY_REFERENCE="v0.1.0.dev1"
 RUBIN_NIGHTS_REFERENCE="v0.3.0"
 
 pip install --no-deps --target=${PACKAGE_DIR} \
